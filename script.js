cb17a862fcd20cc46b4498cc932edc65773dc270
Juan Ignacio Castelli <juanicaste23@gmail.com>
2021-07-13 14:37:06 -0300

    Update Script.js (Version 2)
    With the help by Josué

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 /*variables*/
+
 const form = document.getElementById('form');
 const fname = document.getElementById('fname');
 const lname = document.getElementById('lname');
@@ -7,62 +8,76 @@ const password = document.getElementById('password');
 const eyeicon = document.querySelector('.eye-icon');
 
 /*funciones*/
+
+//funcion que demuestra que el input fue ingresado equivocadamente
 const showError = (input, message) => {
+    
     const formControl = input.parentElement;
     input.classList.add('input-error');
     input.removeAttribute('placeholder');
     formControl.children[1].style.display = "block";
     formControl.children[2].style.display = "block";
     formControl.children[2].innerText = message;
+    
 };
 
+//funcion que demuestra que el input fue ingresado correctamente
 const showSuccess = (input) => {
+    
     const formControl = input.parentElement;
     input.classList.remove('input-error');
     formControl.children[1].style.display = 'none';
     formControl.children[2].style.display = 'none';
+    
 };
 
+//funcion que onvierte texto a uppercase
 const getFieldName = (input) => {
+    
     const name = input.name;
     return name.replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase())));
-
-    //(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase()))); expresion regular para convertir texto a Uppercase
+    
 };
 
-//Validar email
+//funcion que valida email
 const checkEmail = (input) => {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    ///^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; Expresion regular para validar emails en un formulario GROSO!
-
-    if( re.test(input.value.trim()) ) {
+    if( re.test(input.value.trim()) ) 
+    {
         showSuccess(input);
-    } else {
+    } 
+    else 
+    {
         showError(input, 'Looks like this is not an email');
         input.value = "email@example/com";
     }
 
 };
 
-//validar campo requerido
+//funcion que valida los input con las funciones showError y showSuccess
 const checkRequired = ( inputArr ) => {
 
     inputArr.forEach( input => {
         
-        if ( input.value.trim() === '') {
+        if ( input.value.trim() === '') 
+        {
             showError(input, `${getFieldName(input)} cannot be empty`);
             password.type = 'password';
-        } else {
+        } 
+        else 
+        {
             showSuccess(input);
         }   
         
     });
 };
 
-/*hacer una función que haga visible el password*/
+//funcion que revela la password con el ojito, y tiene un innerhtml
 const revelatePassword = (e) => {
+   
     const insertText = document.getElementById('innerhtml');
+    
     if(password.type === 'password'){
         password.type = 'text';
         eyeicon.style.backgroundImage = "url('images/visibility_black_24dp.svg')";
@@ -75,22 +90,19 @@ const revelatePassword = (e) => {
         innerhtml.innerHTML = 'Hidden';
         innerhtml.style.color = "var(--red)"
     }
+    
 };
 
-/*Funcion para llamar a los eventos*/
+//funcion para llamar a los eventos de los input
 const functionForm = (e) => {
+    
     e.preventDefault();
     checkRequired([fname, lname, email, password]);
     checkEmail(email);
+    
 };
 
-// //validar tamaño del string
-// const checkLength = (input) => {
-
-//     if(input.value.trim().length <= 15)
-
-// }
-
 /*eventos*/
+
 form.addEventListener('submit',functionForm);
 eyeicon.addEventListener('click',revelatePassword);
